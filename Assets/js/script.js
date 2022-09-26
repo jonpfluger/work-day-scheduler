7b2079530382fe75d1a8d510721df7dc28b76644
jonpfluger <jonpfluger718@gmail.com>
2022-09-26 11:00:58 -0500

    localStorage on save buttons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,72 +3,40 @@ var currentDayEl = document.getElementById('currentDay');
 var now = moment().format("ddd MM, YYYY HH:mm a");
 currentDayEl.innerText = now;
 
-var inputEl = document.querySelectorAll('input');
+// changes class based on past present or future
+$('.text-area').each(function() {
+    var textAreaTime = $(this).attr('id');
+    var currentTime = moment().hours();
+    if (textAreaTime < currentTime) {
+        $(this).addClass('past')
+    } else if (textAreaTime > currentTime) {
+        $(this).addClass('future')
+    } else {
+        $(this).addClass('present')
+    }
+})
 
+// 
+var saveBtnEl = document.querySelectorAll('button');
 
+function save() {
+    var textAreaTime = $(this).siblings('input').attr('id')
+    var textAreaInput = $(this).siblings('input').val()
+    localStorage.setItem(textAreaTime, textAreaInput)
+    
+}
 
-//Change textarea background color based on time
-function checkTime() {
-
-
-
-    //loop through taskarea classes
-    for (var i = 0 ; i < timeBlockElements.length ; i++) {
-
-
-        //remove any old classes from element
-        .removeClass(".present .past .future");
-
-        // apply new class if task is present/past/future
-        if (timeEl < now) {
-            inputEl.classList.add("past");
-        } else if (timeEl > now) {
-            inputEl.classList.add("future");
-        } else {
-            inputEl.classList.add("present");
-        }
+// adds click listener to save buttons
+function clickEvent() {
+    for (var i = 0; i < saveBtnEl.length; i++) {
+        saveBtnEl[i].addEventListener('click', save);
     }
 }
 
-// checkTime every 5 minutes
-setInterval(checkTime(), (1000 * 60) * 5);
+clickEvent();
 
 
-
-
-
-// var hour = document.getElementsByClassName('hour');
-
-// function checkTime () {
-
-//     var time = moment(hour, "LT");
-//     console.log(time)
-//     // removes the past/present/future classes
-//     hour.removeClass(".present .past .future");
-//     // if (past)
-//     if (moment().isAfter(time)) {
-//         hour.addClass(".past");
-//     // else if (future)
-//     } else if (moment().isBefore(time)) {
-//         hour.addClass(".future");
-//     // else present
-//     } else {
-//         hour.addClass(".present");
-//     }
-// }
-
-
-
-
-// removes the past/present/future classes
-// $(".hour").removeClass(".present .past .future");
-
-// if (past) {
-    // set class to past
-// if (future) {
-    // set class to future (on input)
-// else
-    // set class to present
-
-
-// click listener to save buttons that saves it to local storage
+for (let i = 9; i <= 17; i++) {
+    $('#' + i).val(localStorage.getItem(i))
+     
+}
